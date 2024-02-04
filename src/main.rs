f14f83aee78900eecd6faf9384577fd11e60575d
ckvj <carlkvogel@gmail.com>
2024-02-04 15:20:26 -0500

    added capability to copy text to OS clipboard. Other small fixes

--- src/main.rs
+++ src/main.rs
@@ -5,17 +5,17 @@ fn main() {
     println!("Enter First Address");
     let string1 = get_string_input(); 
 
+    let interm_string = "Interm string copied to Clipboard";
+    cli_clipboard::set_contents(interm_string.to_owned()).unwrap();
+
     println!("Enter Second Address");
     let string2 = get_string_input(); 
 
-    match str_comp(&string1, &string2) {
-        true => {
+    if str_comp_sensitive(&string1, &string2) {
             println!("{}", "Case Sensitive: MATCH".on_green());
             println!("Matched String: {}",&string1.green());
             process::exit(0)
-        },
-        false => println!("Case Sensitive: NO MATCH"),
-    }
+    };
 
     match str_comp_insensitive(string1, string2) {
         true => println!("{}", "Case Insensitive: MATCH".on_yellow()),
@@ -29,7 +29,7 @@ fn get_string_input() -> String {
     input.trim().to_string()
 }
 
-fn str_comp(string1: &str, string2: &str) -> bool {
+fn str_comp_sensitive(string1: &str, string2: &str) -> bool {
     string1.eq(string2)
 }
 
